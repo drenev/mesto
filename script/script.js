213bda33bcd1e51147b27ab7b82edec38de9ec36
safelbob <dimas.renyow@yandex.ru>
2021-04-05 18:36:43 +0300

    change varibles

--- script/script.js
+++ script/script.js
@@ -1,21 +1,21 @@
-const editForm = document.querySelector('.edit-form');
-const editButton = document.querySelector('.profile__edit-button');
+let editForm = document.querySelector('.edit-form');
+let editButton = document.querySelector('.profile__edit-button');
 editButton.addEventListener("click", function openPopup() {
     editForm.classList.add("edit-form_opened")
 });
 
-const closeButton = document.querySelector('.edit-form__close-icon');
+let closeButton = document.querySelector('.edit-form__close-icon');
 
 closeButton.addEventListener("click", function closePopup() {
     editForm.classList.remove("edit-form_opened")
 });
 
-const formName = document.querySelector('.edit-form__name');
-const formDescription = document.querySelector('.edit-form__description');
-const saveButton = document.querySelector('.edit-form__button');
+let formName = document.querySelector('.edit-form__name');
+let formDescription = document.querySelector('.edit-form__description');
+let saveButton = document.querySelector('.edit-form__button');
 
-const profileName = document.querySelector('.profile__username');
-const profileDescription = document.querySelector('.profile__description');
+let profileName = document.querySelector('.profile__username');
+let profileDescription = document.querySelector('.profile__description');
 
 formName.value = profileName.textContent;
 formDescription.value = profileDescription.textContent;
