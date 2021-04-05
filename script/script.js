7b8b5ab6e24c90d42244e7f55f7560cc5594317a
safelbob <dimas.renyow@yandex.ru>
2021-04-05 18:28:35 +0300

    add textContent vaule

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
+const editForm = document.querySelector('.edit-form');
 const editButton = document.querySelector('.profile__edit-button');
 editButton.addEventListener("click", function openPopup() {
-    document.querySelector('.edit-form').classList.add("edit-form_opened")
+    editForm.classList.add("edit-form_opened")
 });
 
 const closeButton = document.querySelector('.edit-form__close-icon');
+
 closeButton.addEventListener("click", function closePopup() {
-    document.querySelector('.edit-form').classList.remove("edit-form_opened")
+    editForm.classList.remove("edit-form_opened")
 });
 
 const formName = document.querySelector('.edit-form__name');
@@ -15,8 +17,11 @@ const saveButton = document.querySelector('.edit-form__button');
 const profileName = document.querySelector('.profile__username');
 const profileDescription = document.querySelector('.profile__description');
 
+formName.value = profileName.textContent;
+formDescription.value = profileDescription.textContent;
+
 saveButton.addEventListener("click", function saveInfo() {
     profileName.innerHTML = formName.value;
     profileDescription.innerHTML = formDescription.value;
-    document.querySelector('.edit-form').classList.remove("edit-form_opened");
+    editForm.classList.remove("edit-form_opened");
 });
